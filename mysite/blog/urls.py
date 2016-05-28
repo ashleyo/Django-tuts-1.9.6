4a4cbb9186ad81d42a8dd2405218403af6f80f9b
ashleyo <Ashley.P.Oliver@gmail.com>
2016-05-28 17:38:24 +0100

    CfE videos to end of Model Forms and Create View

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
+    #url(r'^detail$', views.post_detail, name='detail'), #default maybe?
+    url(r'^(?P<id>\d+)$', views.post_detail, name='detail'),
     url(r'^create$', views.post_create, name='create'),
-    url(r'^detail$', views.post_detail, name='detail'), 
-    url(r'^detail/(?P<postTitle>[^/]+)$', views.post_detail, name='detail'),
-    url(r'^update$', views.post_update, name='update'),
+    url(r'^(?P<id>\d+)/edit/$', views.post_update, name='update'),
     url(r'^delete$', views.post_delete, name='delete'),
     url(r'^$', views.post_list, name='list'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,21 +1,41 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def post_create(request):
-    return HttpResponse("<h1>Create</h1>")
+    form = PostForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.save()
+            return redirect('blog:detail', id = instance.pk)
+    context = { 'form': form }
+    return render(request, 'blog/post_form.html', context)
 
-def post_detail(request,postTitle=""):
-    context = get_object_or_404(Post,title=postTitle)
+def post_detail(request,id=1):
+    context = get_object_or_404(Post,id=id)
     return render(request, 'blog/post_detail.html', {'post':context})
     
 def post_list(request):
     context = Post.objects.all()
     return render(request, 'blog/index.html', {'posts':context})
     
-def post_update(request):
-    return HttpResponse("<h1>Update</h1>")
+def post_update(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, instance=instance)
+    if form.is_valid():
+            instance = form.save(commit=False)
+            instance.save()
+            #return redirect('blog:detail', id = instance.pk)
+            return redirect(instance.get_absolute_url())
+    context = { 
+            'title' : instance.title, 
+            'content' : instance.content,
+            'form' : form,
+            }
+    return render(request, 'blog/post_form.html', context)
 
 def post_delete(request):
     return HttpResponse("<h1>Delete</h1>")
