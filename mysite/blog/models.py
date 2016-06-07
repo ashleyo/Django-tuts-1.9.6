f8caa4076904c19f626963dc6afcd20c1ec0d6f2
Ashley Oliver <ashley.p.oliver@gmail.com>
2016-06-07 14:16:12 +0100

    minor tweak

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.urlresolvers import reverse
-from django.db.models.signals import pre_save
+#from django.db.models.signals import pre_save
 from django.utils.text import slugify
 
 
@@ -43,9 +43,9 @@ def create_slug(instance, new_slug=None) :
         return create_slug(instance, new_slug=new_slug) 
     return slug      
         
-def pre_save_Post_receiver(sender, instance, *args, **kwargs):
-    if not instance.slug:
-        instance.slug = create_slug(instance)
+#def pre_save_Post_receiver(sender, instance, *args, **kwargs):
+#    if not instance.slug:
+#        instance.slug = create_slug(instance)
 
 #pre_save.connect(pre_save_Post_receiver, sender=Post)
     
