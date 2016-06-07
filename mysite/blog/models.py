b3aac6354a9c4a855e2b7e0e2a6751c1d8be0dca
Ashley Oliver <ashley.p.oliver@gmail.com>
2016-06-07 14:14:58 +0100

    Alternative slugify using save override rather than signals

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -24,6 +24,10 @@ class Post(models.Model):
         return self.title
     def get_absolute_url(self):
         return reverse('blog:detail', kwargs={"slug":self.slug})
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = create_slug(self)
+        super().save(*args, **kwargs)
         
     class Meta:
         ordering=["-timestamp","-updated"]
@@ -43,6 +47,6 @@ def pre_save_Post_receiver(sender, instance, *args, **kwargs):
     if not instance.slug:
         instance.slug = create_slug(instance)
 
-pre_save.connect(pre_save_Post_receiver, sender=Post)
+#pre_save.connect(pre_save_Post_receiver, sender=Post)
     
     
