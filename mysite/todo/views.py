a9a3a26b66fbcfbe5420e5cdb4aa0ba9b9e86a49
ashleyo <Ashley.P.Oliver@gmail.com>
2016-06-20 23:07:29 +0100

    fixed typo

--- mysite/todo/views.py
+++ mysite/todo/views.py
@@ -4,4 +4,4 @@ from .models import ToDoItem
 class ListView(GenericListView):
     context_object_name = 'todos'
     queryset = ToDoItem.objects.order_by('target_date')
-    template_name = 'todo/todo_list.html''
+    template_name = 'todo/todo_list.html'
